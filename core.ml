776d5a7828a32f030f0c7a0874c20ccad20b0752
pranav-159 <cs20btech11018@iith.ac.in>
2022-12-16 15:50:17 +0530

    state of art

--- core.ml
+++ core.ml
@@ -1,15 +1,60 @@
 open Printf;;
-let n = 6;;
-let m = 6;;
+let n = 1501;;
+let m = 1;;
+let seconds = 0.02;;
+let fname = "testfile.txt";;
 
 (* making adjacency list *)
 let adj_list = Array.make n [];;
 
-adj_list.(1) <- [2;3] ;
+(* in_degree for undirected graph *)
+let deg = Array.make n (0,0);;
+let file = open_in fname in
+let quit_loop = ref false in
+  while not !quit_loop do
+    try
+    let line = input_line file in
+      let ls = List.map int_of_string (String.split_on_char ' ' line) in 
+        match ls with
+        | [] -> () 
+        | h::t -> deg.(h) <- (h,List.length t)
+    with End_of_file -> quit_loop := true
+  done ;
+  close_in file
+;;
+(* sort the degree using stable sort *)
+let lex_compare (x,y) (x',y') = compare y' y
+;;
+Array.stable_sort lex_compare deg;;
+
+let phi = Array.map (fun (x,y) -> x) deg;;
+
+
+
+let rec screener a l=
+  match l with
+  [] -> []
+  | h::t -> if phi.(h)>a then phi.(h)::screener a t else screener a t
+
+
+  (* making a core for test *)
+let file = open_in "test.txt";;
+let quit_loop = ref false in
+  while not !quit_loop do
+    try
+    let line = input_line file in
+      let ls = List.map int_of_string (String.split_on_char ' ' line) in 
+        let h::t = ls in
+        adj_list.(phi.(h)) <- screener phi.(h) t
+    with End_of_file -> quit_loop := true
+  done;;
+close_in file;;
+
+(* adj_list.(1) <- [2;3] ;
 adj_list.(2) <- [4] ;
 adj_list.(3) <- [4;5] ;
 adj_list.(4) <- [] ;
-adj_list.(5) <- [];;
+adj_list.(5) <- [];; *)
 
 (* function to make in-degree array *)
 (* makes an atomic in_degree array from adjacency list*)
@@ -44,18 +89,20 @@ let pool = T.setup_pool ~num_additional_domains:7 ();; *)
 
 (* let fun_queue =  Queue.empty;; *)
 
-let functions = Lockfree.Mpmc_relaxed_queue.create ~size_exponent:3 ()
+let functions = Lockfree.Mpmc_relaxed_queue.create ~size_exponent:11 ()
 ;;
 module MPMCQueue = Lockfree.Mpmc_relaxed_queue.Not_lockfree;;
 
 let counter = Atomic.make 0;;
 
+let minisleep (sec: float) =
+  ignore (Unix.select [] [] [] sec)
 let rec execute x =
   printf "started %d\n%!" x;
-  Unix.sleep 2;
+  minisleep seconds;
   let ls = adj_list.(x) in
-  List.iter (fun x -> in_degree.(x) <- in_degree.(x)-1; 
-  if in_degree.(x) = 0 then ignore (MPMCQueue.push functions x)
+  List.iter (fun x -> 
+  if  (Atomic.fetch_and_add in_degree.(x) (-1)) = 1 then ignore (MPMCQueue.push functions x)
    ) 
   ls;
   printf "ended %d\n%!" x;
@@ -78,7 +125,7 @@ let thread_loop () =
 
 let main () =
   for i = 1 to (n-1) do
-    if in_degree.(i) = 0 then ignore (MPMCQueue.push functions i) 
+    if Atomic.get in_degree.(i) = 0 then ignore (MPMCQueue.push functions i) 
   done;
   print_newline ();
   let p = Array.make m (Domain.spawn (fun () -> thread_loop ()))  in
